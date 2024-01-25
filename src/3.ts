b5e3c792afa618a908be7a8d762d5dcb6d385ffe
Anhelina Rabtsevych <anhelina@ondaytrip.com>
2024-01-25 23:59:30 +0100

    solutions

--- src/3.ts
+++ src/3.ts
@@ -7,7 +7,20 @@
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
 
-// реалізація класу Wizard
+// Оголошення інтерфейсів
+interface ICharacter {
+  name: string;
+  level: number;
+
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
+
+interface ISpellCaster {
+  castSpell(): void;
+}
+
+// Реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
@@ -29,7 +42,7 @@ class Wizard implements ICharacter, ISpellCaster {
   }
 }
 
-// тестування класу
+// Тестування класу
 const wizard = new Wizard('Merlin', 15);
 
 wizard.introduce('I am the mighty wizard');
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,73 @@
-const key = new Key();
+// class key
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  
+  // class person
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  // class house
+  abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    abstract openDoor(key: Key): void;
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log('Person entered the house.');
+      } else {
+        console.log('The door is closed. Person cannot enter the house.');
+      }
+    }
+  }
+  
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (this.key.getSignature() === key.getSignature()) {
+        this.door = true;
+        console.log('Door opened.');
+      } else {
+        console.log('Access denied. Wrong key.');
+      }
+    }
+  }
+  
 
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+  const key: Key = new Key();
+  const person: Person = new Person(key);
+  const house: MyHouse = new MyHouse(key);
+  
+  house.openDoor(person.getKey());
+  house.comeIn(person);
+  
+  console.log(house);
+  
+  export {};
+  
